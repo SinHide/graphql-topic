81dd4ebb2c15113014cd890e99c2f5f8aaa7505d
SinHide <yyxiejunfeng@163.com>
2018-04-12 12:25:30 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './resources/logo.svg';
+import './App.less';
 
+const ChannelList = () => 
+  ( <ul className='item-list'>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul> );
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Apollo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChannelList />
       </div>
     );
   }
